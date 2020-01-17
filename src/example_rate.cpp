e076306a107bfd7014ea047604dcefbca361a68a
rxu74 <ruoyangx@qq.com>
2020-01-17 13:36:58 -0500

    example usage

--- src/example_rate.cpp
+++ src/example_rate.cpp
@@ -1,9 +1,6 @@
 
 #include "ros/ros.h"
-#include "ros/console.h"
-#include "benchmarking_tools/rate_tracker.h"
-#include "benchmarking_tools/rate.h"
-#include "benchmarking_tools/tracker.h"
+#include "benchmarking_tools/macros.h"
 #include "std_msgs/String.h"
 
 
@@ -12,11 +9,14 @@ int main(int argc, char ** argv)
     ros::init(argc, argv, "rate_tester");
     ros::NodeHandle n;
     ros::Rate loop_rate(2);
-
-    Tracker<Rate> tracker_obj("temp_name", 50, ros::console::levels::Info);
-
+    std_msgs::Header last_msgs;
+    last_msgs.stamp = ros::Time::now();
     while (ros::ok()){
-        
+        RATE_LOG("example_rate node", ros::console::Level::Info, 50);
+        ros::Duration(0.05).sleep();
+        ros::spinOnce();
+        DELAY_LOG("delay log", ros::console::Level::Info, 50, last_msgs);
+        last_msgs.stamp = ros::Time::now();
     }
     return 0;
 }
--- src/example_timing.cpp
+++ src/example_timing.cpp
@@ -1,6 +1,5 @@
 #include "ros/ros.h"
-#include "benchmarking_tools/timing.h"
-#include "benchmarking_tools/rate_tracker.h"
+#include "benchmarking_tools/macros.h"
 #include "std_msgs/String.h"
 
 
@@ -11,18 +10,8 @@ int main(int argc, char ** argv)
     ros::Rate loop_rate(2);
 
     while (ros::ok()){
-        {
-            TIMING("logger1","INFO");
-            ros::Duration(0.05).sleep();
-        }
-
-        {
-            TIMING("logger2", "INFO");
-            ros::Duration(0.05).sleep();
-        }
-
-        RATE_TRACKING_INFO_NAMED(50, "example_rate");
-
+        RATE_LOG("example_rate node_2", ros::console::Level::Info, 50);
+        ros::Duration(0.5).sleep();
         ros::spinOnce();
     }
     return 0;
